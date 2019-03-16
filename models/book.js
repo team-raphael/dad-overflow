20f8238c2ac04bd1e4dce425146b24af563a8fbc
min <mjnorthwesternwork1121@gmail.com>
2019-03-16 11:49:06 -0500

    updates

--- models/book.js
+++ models/book.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-  bookId: { type: String, required: true, unique: true },
-  smallThumbnail: String,
-  infoLink: String,
-  title: String,
-  authors: Array,
-  description: String,
-  date: { type: Date, default: Date.now }
-});
+//EXAMPLE Schema
+//const bookSchema = new Schema({
+  //bookId: { type: String, required: true, unique: true },
+  //smallThumbnail: String,
+  //infoLink: String,
+  //title: String,
+  //authors: Array,
+  //description: String,
+  //date: { type: Date, default: Date.now }
+//});
+
+const postsSchema( {});
+
+const commentsSchema({});
+
+const todosSchema({});
+
+const userSchema({});
 
 const Book = mongoose.model("Book", bookSchema);
 
