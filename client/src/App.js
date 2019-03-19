a6073508e9759fea820759618b6996079a7dd649
Noel Nevarez <noel3740@gmail.com>
2019-03-19 11:09:47 -0500

    Redirect user to profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Nav from './components/Nav';
 import Forum from "./pages/Forum";
 import ToDo from './pages/ToDo';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 import './App.css';
 import FirebaseContext from './components/Firebase/context';
 import * as FirebaseApp from 'firebase/app';
 import API from './services/APIService';
 
 class App extends Component {
+
+  state = {
+    toProfile: false
+  };
 
   componentDidMount = () => {
     window.ioSocket.on('message', function (msg) {
@@ -20,11 +25,26 @@ class App extends Component {
     //Attach an on authoriztion change envent and send the user to the profile page if they don't exist in the datbase
     FirebaseApp.auth().onAuthStateChanged((user) => {
 
+      this.firebase.userInfo = {};
+
       if (user) {
-        console.log("Firebase User Info: ", user);
-        API.getAllUsers()
-          .then(users => {
-            console.log(users);
+        API.getUserByEmail(user.email)
+          .then(dbUsers => {
+
+            if (dbUsers.data.length > 0) {
+              this.firebase.userInfo = {
+                db: dbUsers.data[0],
+                firebase: user
+              }
+            } else {
+              //Redirect the user to the profile page
+              this.setState({
+                toProfile: true
+              });
+            }
+
+            console.log("DB User Info: ", this.firebase.userInfo);
+            //this.props.history.push(`/profile`);
           })
           .catch(err => {
             console.log("Trouble obtaining the user data from the database: ", err);
@@ -41,6 +61,16 @@ class App extends Component {
           firebase => {
             this.firebase = firebase;
 
+            if (this.state.toProfile === true) {
+              return (
+                <BrowserRouter>
+                  <div className="App">
+                    <Redirect to="/profile" />
+                  </div>
+                </BrowserRouter>
+              )
+            }
+
             return (
               <BrowserRouter>
                 <div className="App">
@@ -50,11 +80,14 @@ class App extends Component {
                     <Route exact path="/" component={Forum} />
                     <Route exact path="/todo" component={ToDo} />
                     <Route exact path="/login" component={Login} />
+                    <Route exact path="/profile" component={Profile} />
                     <Route component={NoMatch} />
                   </Switch>
+
                 </div>
               </BrowserRouter>
             )
+
           }
         }
 
