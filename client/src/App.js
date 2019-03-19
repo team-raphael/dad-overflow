ba278332e625194a14b5cc37c5a537a0cee99e39
Noel Nevarez <noel3740@gmail.com>
2019-03-19 10:10:58 -0500

    Adding authoriztion state event listener

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,30 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import './App.css';
 import FirebaseContext from './components/Firebase/context';
+import * as FirebaseApp from 'firebase/app';
+import API from './services/APIService';
 
 class App extends Component {
-
 
   componentDidMount = () => {
     window.ioSocket.on('message', function (msg) {
       window.M.toast({ html: msg });
+    });
+
+    //Attach an on authoriztion change envent and send the user to the profile page if they don't exist in the datbase
+    FirebaseApp.auth().onAuthStateChanged((user) => {
+
+      if (user) {
+        console.log("Firebase User Info: ", user);
+        API.getAllUsers()
+          .then(users => {
+            console.log(users);
+          })
+          .catch(err => {
+            console.log("Trouble obtaining the user data from the database: ", err);
+          })
+      }
+
     });
   }
 
