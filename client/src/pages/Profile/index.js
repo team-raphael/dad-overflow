635773563bcfb3652f59e7f840f239a37176bd5c
Noel Nevarez <noel3740@gmail.com>
2019-03-19 10:10:42 -0500

    Adding profile page.

---
+++ client/src/pages/Profile/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+class Profile extends React.Component {
+
+    render() {
+        return (
+            <div className="white-text">
+
+                <div className="pageContainer">
+                    <div className="container">
+                        <h1>This is the profile page</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Profile;
