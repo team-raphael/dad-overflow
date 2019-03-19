c0983988565ce97d6b9227e3598f5c82a88cdf78
Noel Nevarez <noel3740@gmail.com>
2019-03-19 09:17:59 -0500

    Adding UserId reference

--- models/task.js
+++ models/task.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-  
-  body: String,
-  isComplete: { type: Boolean, required: true, default: false },
-  date: { type: Date, default: Date.now }
 
+  body: String,
+  isComplete: { type: Boolean, required: true, default: "false" },
+  date: { type: Date, default: Date.now },
+  //Associate the _id from the User model to the task
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 const Task = mongoose.model("Task", taskSchema);
