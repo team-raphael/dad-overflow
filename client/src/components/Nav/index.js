52d51da8ff2cb4f335a6d12870cac126ff0609fa
Noel Nevarez <noel3740@gmail.com>
2019-03-20 13:44:22 -0500

    Cleaning up code to use the FirebaseContext

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './style.css';
 import * as FirebaseApp from 'firebase/app';
-import API from '../../services/APIService';
+import profileBackground from '../../backgroundImage1.jpg';
+import FirebaseContext from '../Firebase/context';
 
 class Nav extends React.Component {
-
-    state = {
-        currentUser: null,
-        dbUser: null
-    };
 
     sideNavInstance = null;
 
@@ -23,45 +19,6 @@ class Nav extends React.Component {
         //Initialize the nav bar
         const sideNavElement = document.querySelector('.sidenav');
         this.sideNavInstance = window.M.Sidenav.init(sideNavElement);
-
-        //Initialize profile drop down
-        var elems = document.querySelectorAll('.dropdown-trigger');
-        window.M.Dropdown.init(elems);
-
-        //Setup a listener for when the user's login state changes so we can change what is visible on the nav bar
-        FirebaseApp.auth().onAuthStateChanged((user) => {
-
-            this.setState({
-                currentUser: user
-            });
-
-            //Check the database for this user and set the state to that user
-            if (user) {
-                API.getUserByEmail(user.email)
-                    .then(dbUsers => {
-                        if (dbUsers.data && dbUsers.data.length > 0) {
-                            this.setState({
-                                dbUser: dbUsers.data[0]
-                            });
-                        } else {
-                            this.setState({
-                                dbUser: null
-                            });
-                        }
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        window.M.toast({ html: 'Error obtaining user from the database!' });
-                    });
-            }
-
-        }, (error) => {
-            console.log(error);
-
-            this.setState({
-                currentUser: null
-            });
-        });
     };
 
     //Method that closes the side nav
@@ -77,67 +34,95 @@ class Nav extends React.Component {
     };
 
     render() {
+
         return (
-            <div className="reactNav">
-                <div className="navbar-fixed">
-                    <nav>
-                        <div className="container nav-wrapper">
-                            <Link to={"/"} className="brand-logo"><i className="logo"></i> Dad Overflow</Link>
-                            <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                            <ul className="right hide-on-med-and-down">
-                                <li className={window.location.pathname.toLowerCase() === "/" ? "active" : ""}>
-                                    <Link to={"/"}>Forum</Link>
-                                </li>
-                                <li className={window.location.pathname.toLowerCase() === "/todo" ? "active" : ""}>
-                                    <Link to={"/todo"}>To Do</Link>
-                                </li>
-                                {!this.state.currentUser &&
-                                    <li className={`loginLink ${window.location.pathname.toLowerCase() === "/login" ? "active" : ""}`}>
-                                        <Link className="waves-effect waves-light btn" to={"/login"}>Login</Link>
-                                    </li>
-                                }
-                                {this.state.currentUser &&
+            <FirebaseContext.Consumer>
+                {
+                    firebase => {
+
+                        return (
+                            <div className="reactNav">
+                                <div className="navbar-fixed">
+                                    <nav>
+                                        <div className="container nav-wrapper">
+                                            <Link to={"/"} className="brand-logo"><i className="logo"></i> Dad Overflow</Link>
+                                            <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                                            <ul className="right hide-on-med-and-down">
+                                                <li className={window.location.pathname.toLowerCase() === "/" ? "active" : ""}>
+                                                    <Link to={"/"}>Forum</Link>
+                                                </li>
+                                                <li className={window.location.pathname.toLowerCase() === "/todo" ? "active" : ""}>
+                                                    <Link to={"/todo"}>To Do</Link>
+                                                </li>
+                                                {!firebase.firebaseUserInfo &&
+                                                    <li className={`loginLink ${window.location.pathname.toLowerCase() === "/login" ? "active" : ""}`}>
+                                                        <Link className="waves-effect waves-light btn" to={"/login"}>Login</Link>
+                                                    </li>
+                                                }
+                                                {firebase.firebaseUserInfo &&
+                                                    <li>
+                                                        <img className="dropdown-trigger profileImage" data-target='profileDropdown' src={(firebase.dbUserInfo && firebase.dbUserInfo.image) ? firebase.dbUserInfo.image : "https://via.placeholder.com/225"} alt="profile"></img>
+
+                                                        <ul id='profileDropdown' className='dropdown-content'>
+                                                            <li>
+                                                                <Link className="profileLink" to={"/profile"}>Profile</Link>
+                                                            </li>
+                                                            <li className="divider" tabIndex="-1"></li>
+                                                            <li className="noHighlight">
+                                                                <button onClick={this.signOutOnClick} className="signOffButton waves-effect waves-light btn">Sign Off</button>
+                                                            </li>
+                                                        </ul>
+                                                    </li>
+                                                }
+                                            </ul>
+                                        </div>
+                                    </nav>
+
+                                </div>
+
+                                <ul className="sidenav" id="mobile-demo">
                                     <li>
-                                        <img className="dropdown-trigger profileImage" data-target='profileDropdown' src={(this.state.dbUser && this.state.dbUser.image) ? this.state.dbUser.image : "https://via.placeholder.com/225"} alt="profile"></img>
-
-                                        <ul id='profileDropdown' className='dropdown-content'>
-                                            <li>
-                                                <Link className="profileLink" to={"/profile"}>Profile</Link>
-                                            </li>
-                                            <li className="divider" tabIndex="-1"></li>
-                                            <li className="noHighlight">
-                                                <button onClick={this.signOutOnClick} className="signOffButton waves-effect waves-light btn">Sign Off</button>
-                                            </li>
-                                        </ul>
+                                        <div className="user-view">
+                                            <div className="background">
+                                                <img src={profileBackground} alt="background" />
+                                            </div>
+                                            <div><img className="circle" data-target='profileDropdown' src={(firebase.dbUserInfo && firebase.dbUserInfo.image) ? firebase.dbUserInfo.image : "https://via.placeholder.com/225"} alt="profile" /></div>
+                                            <div><span className="white-text name">{(firebase.firebaseUserInfo && firebase.firebaseUserInfo.displayName) ? firebase.firebaseUserInfo.displayName : ""}</span></div>
+                                            <div><span className="white-text email">{(firebase.firebaseUserInfo && firebase.firebaseUserInfo.email) ? firebase.firebaseUserInfo.email : ""}</span></div>
+                                        </div>
                                     </li>
-                                }
-                            </ul>
-                        </div>
-                    </nav>
 
-                </div>
+                                    <li className={window.location.pathname.toLowerCase() === "/" ? "active" : ""}>
+                                        <Link to={"/"} onClick={this.closeSideNav}>Forum</Link>
+                                    </li>
+                                    <li className={window.location.pathname.toLowerCase() === "/todo" ? "active" : ""}>
+                                        <Link to={"/todo"} onClick={this.closeSideNav}>To Do</Link>
+                                    </li>
+                                    {!firebase.firebaseUserInfo &&
+                                        <li className={window.location.pathname.toLowerCase() === "/login" ? "active" : ""}>
+                                            <Link className="waves-effect waves-light btn" to={"/login"} onClick={this.closeSideNav}>Login</Link>
+                                        </li>
+                                    }
+                                    {firebase.firebaseUserInfo &&
+                                        <li className={window.location.pathname.toLowerCase() === "/profile" ? "active" : ""}>
+                                            <Link to={"/profile"} onClick={this.closeSideNav}>Profile</Link>
+                                        </li>
+                                    }
+                                    {firebase.firebaseUserInfo &&
+                                        <li>
+                                            <a onClick={this.signOutOnClick} className="signOffButton waves-effect waves-light btn" href="#!">Sign Off</a>
+                                        </li>
+                                    }
+                                </ul>
 
-                <ul className="sidenav" id="mobile-demo">
-                    <li className={window.location.pathname.toLowerCase() === "/" ? "active" : ""}>
-                        <Link to={"/"} onClick={this.closeSideNav}>Forum</Link>
-                    </li>
-                    <li className={window.location.pathname.toLowerCase() === "/todo" ? "active" : ""}>
-                        <Link to={"/todo"} onClick={this.closeSideNav}>To Do</Link>
-                    </li>
-                    {!this.state.currentUser &&
-                        <li className={window.location.pathname.toLowerCase() === "/login" ? "active" : ""}>
-                            <Link className="waves-effect waves-light btn" to={"/login"} onClick={this.closeSideNav}>Login</Link>
-                        </li>
+                            </div>
+                        );
                     }
-                    {this.state.currentUser &&
-                        <li>
-                            <a onClick={this.signOutOnClick} className="signOffButton waves-effect waves-light btn" href="#!">Sign Off</a>
-                        </li>
-                    }
-                </ul>
+                }
 
-            </div>
+            </FirebaseContext.Consumer>
         );
+
     }
 }
 
--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import './style.css';
 import API from '../../services/APIService';
 import LockScreen from '../../components/LockScreen';
-import * as FirebaseApp from 'firebase/app';
+import FirebaseContext from '../../components/Firebase/context';
 
 class Profile extends React.Component {
-
-    state = {
-        userExistsInDB: false
-    };
 
     //Function that will require all the images into the code
     requireAll = requireContext => {
@@ -19,60 +15,27 @@ class Profile extends React.Component {
     images = this.requireAll(require.context('../../profileIcons', false, /\.(png|jpe?g|svg)$/));
 
     componentDidMount = () => {
+        this.initializeCarousel();
+    }
+
+    componentDidUpdate = () => {
+        this.initializeCarousel();
+    }
+
+    initializeCarousel = () => {
         //Initialize the materialize carousel
         var elems = document.querySelectorAll('.carousel');
         window.M.Carousel.init(elems);
         this.profileImageCarouselInstance = window.M.Carousel.getInstance(document.querySelector('#profileImageCarousel'));
 
-        //Lock the screen until the firebase authentication has been obtained
-        this.lockScreen.lock();
+        //Set the carousel item image to whatever the user has in the database
+        if (this.firebase.dbUserInfo && this.firebase.dbUserInfo.image) {
 
-        //Attach on authentication event listener to prepopulate screen data
-        FirebaseApp.auth().onAuthStateChanged(this.firebaseAuthenticated);
-    }
-
-    firebaseAuthenticated = (user) => {
-
-        if (user) {
-            //If the user is in the database already then initialize the screen with values from the database
-            API.getUserByEmail(user.email)
-                .then(dbUsers => {
-                    this.firebaseUserInfo = user;
-
-                    if (dbUsers.data.length > 0) {
-                        document.getElementById('profileForm').classList.remove('hide');
-                        document.getElementById('noDBUser').classList.add('hide');
-
-                        this.dbUserInfo = dbUsers.data[0];
-
-                        //If the user has an image in the database then set the image on the screen to match
-                        const currentUserImage = dbUsers.data[0].image;
-                        if (currentUserImage) {
-
-                            document.querySelectorAll('.carousel-item')
-                                .forEach((carouselItem, index) => {
-                                    if (carouselItem.firstChild.getAttribute("src") === currentUserImage) {
-                                        this.profileImageCarouselInstance.set(index);
-                                    }
-                                });
-                        }
-                    } else {
-                        document.getElementById('profileForm').classList.add('hide');
-                        document.getElementById('noDBUser').classList.remove('hide');
-                        window.M.toast({ html: "User must exist in the database in order to use this page! Please log off and log back in." });
+            document.querySelectorAll('.carousel-item')
+                .forEach((carouselItem, index) => {
+                    if (carouselItem.firstChild.getAttribute("src") === this.firebase.dbUserInfo.image) {
+                        this.profileImageCarouselInstance.set(index);
                     }
-                })
-                .catch(err => {
-                    this.setState({
-                        dbUserInfo: null
-                    });
-
-                    document.getElementById('profileForm').classList.add('hide');
-                    window.M.toast({ html: "Trouble obtaining the user data from the database!" });
-                    console.log("Trouble obtaining the user data from the database: ", err);
-                })
-                .finally(() => {
-                    this.lockScreen.unlock();
                 });
         }
     }
@@ -86,13 +49,13 @@ class Profile extends React.Component {
         const selectedImageUrl = selectedImageElement.getAttribute("src");
 
         //If the user already has a database entry then we'll perform an udpate
-        if (this.dbUserInfo) {
+        if (this.firebase.dbUserInfo) {
             const updateUser = {
                 image: selectedImageUrl
             }
 
             //Update the database then redirect the user back to the home screen
-            API.updateUser(this.dbUserInfo._id, updateUser)
+            API.updateUser(this.firebase.dbUserInfo._id, updateUser)
                 .then(() => {
                     //this.props.history.push('/');
                     window.location.href = "/";
@@ -100,42 +63,56 @@ class Profile extends React.Component {
         }
     }
 
+
     render() {
+
         return (
+            <FirebaseContext.Consumer>
+                {
+                    firebase => {
 
-            <div className="profilePage white-text">
-                <div className="pageContainer">
-                    <div className="container">
-                        <div className="row">
-                            <h1>User Profile</h1>
-                        </div>
-                        <div className="row">
-                            <form id="profileForm" className="col s12">
-                                <div className="row">
+                        this.firebase = firebase;
 
-                                    <div className="input-field col l4 m6 s12">
-                                        <div id="profileImageCarousel" className="carousel">
-                                            {this.images.map((image, index) =>
-                                                <a key={index} className="carousel-item" href={`#${index}!`}><img src={image} alt="Misc Baby" /></a>
-                                            )}
+                        return (
+                            <div className="profilePage white-text">
+                                <div className="pageContainer">
+                                    <div className="container">
+                                        <div className="row">
+                                            <h1>User Profile</h1>
                                         </div>
-                                        <label htmlFor="profileImageCarousel">Select your profile image:</label>
-                                    </div>
+                                        <div className="row">
+                                            <form id="profileForm" className={`col s12 ${!firebase.dbUserInfo ? "hide" : ""}`} >
+                                                <div className="row">
 
+                                                    <div className="input-field col l4 m6 s12">
+                                                        <div id="profileImageCarousel" className="carousel">
+                                                            {this.images.map((image, index) =>
+                                                                <div key={index} className="carousel-item"><img src={image} alt="Misc Baby" /></div>
+                                                            )}
+                                                        </div>
+                                                        <label htmlFor="profileImageCarousel">Select your profile image:</label>
+                                                    </div>
+
+                                                </div>
+                                                <div className="row">
+                                                    <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.onSubmit}>Save
+                                                <i className="material-icons right">save</i>
+                                                    </button>
+                                                </div>
+                                            </form>
+                                            <h3 id="noDBUser" className={`col s12 ${firebase.dbUserInfo ? "hide" : ""}`}>User must exist in the database in order to use this page! Please log off and log back in.</h3>
+                                        </div>
+                                    </div>
+                                    <LockScreen id="profilePageLockScreen" ref={(lockScreen) => this.lockScreen = lockScreen} />
                                 </div>
-                                <div className="row">
-                                    <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.onSubmit}>Submit
-                                                <i className="material-icons right">send</i>
-                                    </button>
-                                </div>
-                            </form>
-                            <h3 id="noDBUser" className="hide">User must exist in the database in order to use this page! Please log off and log back in.</h3>
-                        </div>
-                    </div>
-                    <LockScreen id="profilePageLockScreen" ref={(lockScreen) => this.lockScreen = lockScreen} />
-                </div>
-            </div>
-        )
+                            </div>
+                        );
+                    }
+                }
+
+            </FirebaseContext.Consumer>
+        );
+
     }
 }
 
