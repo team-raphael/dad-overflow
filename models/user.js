905b18167c39667452e899ef1c44f9963efcbca3
min <mjnorthwesternwork1121@gmail.com>
2019-03-20 17:00:36 -0500

    adding to schema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,16 @@
+//User//
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//Create an new schema for MongooDB
 const userSchema = new Schema({
+  
   email: { type: String, required: true },
   image: String,
   date: { type: Date, default: Date.now }
+
 });
-
+//Models defined on the mongoose instance are available to all connection created
 const User = mongoose.model("User", userSchema);
-
+//export the Comment schema 
 module.exports = User;
