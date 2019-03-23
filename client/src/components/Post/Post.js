177c3f34be2e9aa5f64e4dbbbd24ddc6df2a070f
cm718 <cm718@icloud.com>
2019-03-23 14:19:57 -0500

    added col and row for image soon to be added

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -5,11 +5,18 @@ const Post = props => {
     return(
 
         <div>
-            <div className='card'>
-                <div className='card-image'>
-                     <h1>{props.title}</h1>
-                     <h3>{props.body}</h3>
-                     <h5>{props.author}</h5>
+            <div className='card container'>
+                <div className='row card-image'>
+
+                        <div className="col l9">
+                            <h1 className='title'>{props.title}</h1>
+                            <h3 className='body'>{props.body}</h3>
+                            <h5 className='author'>{props.author}</h5>
+                         </div>
+                        <div className="col l3">
+                         <img className='hide-on-small' src="" ></img>
+                         </div>
+
                 </div>            
             </div>
         </div>
