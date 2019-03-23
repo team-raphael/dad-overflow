ccebe8259b1af781d74e9a0178399e8c0b627a84
noel3740 <noel3740@gmail.com>
2019-03-23 13:39:57 -0500

    Creating post record in the database.

--- client/src/pages/AddaPost/index.js
+++ client/src/pages/AddaPost/index.js
@@ -1,8 +1,21 @@
 import './style.css';
 import React, { Component } from 'react';
 import FirebaseContext from '../../components/Firebase/context';
+import API from '../../services/APIService';
 
 class AddaPost extends Component {
+
+  state = {
+    postTitle: '',
+    postBody: ''
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
   onSubmit = e => {
     e.preventDefault();
@@ -10,8 +23,18 @@ class AddaPost extends Component {
     const addPostForm = document.querySelector("#addPostForm");
 
     if (addPostForm.checkValidity()) {
+      const newPost = {
+        title: this.state.postTitle,
+        body: this.state.postBody,
+        userId: this.firebase.dbUserInfo._id
+      };
       
-      window.location.href = "/";
+      API.createPost(newPost)
+        .then(() => window.location.href = "/")
+        .catch(err => {
+          console.log(err);
+          window.M.toast({ html: 'Error sending post request' });
+        });
     } else {
       window.M.toast({ html: 'Please enter required fields' });
     }
@@ -28,27 +51,30 @@ class AddaPost extends Component {
             return (
               <div className="white-text">
                 <div className="container">
-                  <form id="addPostForm">
-                    <div className="row">
-                      <div className="input-field col s12">
-                        <input id="last_name" type="text" className="validate" required pattern="^[a-zA-Z1-9].*" />
-                        <label for="last_name">Title</label>
+                  {firebase.dbUserInfo &&
+                    <form id="addPostForm">
+                      <div className="row">
+                        <div className="input-field col s12">
+                          <input id="postTitle" type="text" className="validate" required pattern="^[a-zA-Z1-9].*" name="postTitle" value={this.state.postTitle} onChange={this.handleInputChange} />
+                          <label htmlFor="postTitle">Title</label>
+                        </div>
                       </div>
-                    </div>
-                    <div className="row">
-                      <div className="input-field col s12">
-                        <textarea id="postBody" className="materialize-textarea validate" required pattern="^[a-zA-Z1-9].*"></textarea>
-                        <label for="postBody">Body</label>
+                      <div className="row">
+                        <div className="input-field col s12">
+                          <textarea id="postBody" name="postBody" className="materialize-textarea validate" required pattern="^[a-zA-Z1-9].*" value={this.state.postBody} onChange={this.handleInputChange}></textarea>
+                          <label htmlFor="postBody">Body</label>
+                        </div>
                       </div>
-                    </div>
-                    <div className="row">
-                      <div className="col s12">
-                        <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.onSubmit}>
-                          Submit<i className="material-icons right">send</i>
-                        </button>
+                      <div className="row">
+                        <div className="col s12">
+                          <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.onSubmit}>
+                            Submit<i className="material-icons right">send</i>
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  </form>
+                    </form>
+                  }
+
                 </div>
               </div>
             )
