ad1b9bb328065979c39301b0b0556b90ffc66058
noel3740 <noel3740@gmail.com>
2019-03-23 12:08:54 -0500

    Removing react form with components

--- client/src/pages/AddaPost/index.js
+++ client/src/pages/AddaPost/index.js
@@ -3,63 +3,32 @@ import './style.css';
 import React, { Component } from 'react';
 
 //class FormComponent extends Component {}
- 
 
-const {
-    FormWithConstraints,
-    FieldFeedbacks,
-    FieldFeedback
-  } = ReactFormWithConstraints;
-  
-  class AddaPost extends Component {
-    handleChange = e => {
-      this.form.validateFields(e.target);
+
+class AddaPost extends Component {
+  handleChange = e => {
+    this.form.validateFields(e.target);
+  }
+
+  contactSubmit = e => {
+    e.preventDefault();
+
+    this.form.validateFields();
+
+    if (!this.form.isValid()) {
+      console.log('form is invalid: do not submit');
+    } else {
+      console.log('form is valid: submit');
     }
-  
-    contactSubmit = e => {
-      e.preventDefault();
-  
-      this.form.validateFields();
-  
-      if (!this.form.isValid()) {
-        console.log('form is invalid: do not submit');
-      } else {
-        console.log('form is valid: submit');
-      }
-    }
-  
-    render() {
-      return (
-        <FormWithConstraints>
-          ref={form => this.form = form}
-          onSubmit={this.contactSubmit}
-          noValidate>
-  
-          <div className="col-md-6">
-            <input name="name" size="30" placeholder="Name"
-                   required onChange={this.handleChange}
-                   className="form-control" />
-            <FieldFeedbacks for="name">
-              <FieldFeedback when="*" />
-            </FieldFeedbacks>
-        </div>
-  
-          <div className="col-md-6">
-            <textarea name="comments" cols="40" rows="20" placeholder="Message"
-                      required minLength={5} maxLength={50}
-                      onChange={this.handleChange}
-                      className="form-control" />
-            <FieldFeedbacks for="comments">
-              <FieldFeedback when="*" />
-            </FieldFeedbacks>
-          </div>
-  
-          <div className="col-md-12">
-            <button className="btn btn-lg btn-primary">Send Message</button>
-          </div>
-    </FormWithConstraints>
-      )
-    }
-}
+  }
+
+  render() {
+    return (
+      <form>
         
+      </form>
+    )
+  }
+}
+
 export default AddaPost;
