c24bfbfbdbfe777626b3c2fd2336a1f8942c7da4
Anton Kogan <34128946+chinhippy@users.noreply.github.com>
2019-03-23 13:39:12 -0500

    comment components

---
+++ client/src/components/CommentsList/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export const CommentsList = props => {
+
+  return(
+    <div>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default CommentsList
