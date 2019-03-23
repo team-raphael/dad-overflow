d017e45b8260a1fddee154e3fcc3dfbbb8a8e68e
noel3740 <noel3740@gmail.com>
2019-03-23 13:40:20 -0500

    Changing add post link to point to new page.

--- client/src/components/PostCollection/index.js
+++ client/src/components/PostCollection/index.js
@@ -4,6 +4,7 @@ import API from "../../services/APIService";
 import FirebaseContext from "../Firebase/context";
 import './style.css';
 import Post from '../Post';
+import { Link } from "react-router-dom";
 
 export class PostCollection extends React.Component {
   state = {
@@ -54,13 +55,14 @@ export class PostCollection extends React.Component {
 
           return (
             <div id='modal-btn'>
-              <a
+              <Link to={"/addapost"} className="waves-effect waves-light btn modal-trigger">New Post</Link>
+              {/* <a
                 onClick={this.handleModalClick}
                 class="waves-effect waves-light btn modal-trigger"
-                href="#modal1"
+                href="/addapost"
               >
                 New Post
-              </a>
+              </a> */}
               <Modal id="modal1">
                 {" "}
                 <div class="row">
