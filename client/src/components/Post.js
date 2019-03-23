47aaf4f634312ab4d172ff78eca173281b396e38
cm718 <cm718@icloud.com>
2019-03-23 12:06:30 -0500

    added the title body and author to the card

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Card = styled.card`
-    // width:150px;
-    height:150px;
-    object-fit: cover;
-    overflow: hidden;
-    border: 2px solid #333;
-    border-radius: 6px;
-`
+// const Card = styled.card`
+//     // width:150px;
+//     height:150px;
+//     object-fit: cover;
+//     overflow: hidden;
+//     border: 2px solid #333;
+//     border-radius: 6px;
+// `
 
-const Card = props => {
+const Post = props => {
     return(
 
         <div>
             <div className='card'>
                 <div className='card-image'>
-                     <Card onClick={props.handleClick} />
+                     <h1>{props.title}</h1>
+                     <h3>{props.body}</h3>
+                     <h5>{props.author}</h5>
                 </div>            
             </div>
         </div>
@@ -24,4 +26,4 @@ const Card = props => {
     )
 }
 
-export default Card;
+export default Post;
