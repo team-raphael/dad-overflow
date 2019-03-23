df8e3dd6393f9309f125a1dfb566e9ed8eea2f68
cm718 <cm718@icloud.com>
2019-03-23 13:19:54 -0500

    removed commented items

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,14 +1,5 @@
 import React from 'react';
-// import styled from 'styled-components';
-
-// const Card = styled.card`
-//     // width:150px;
-//     height:150px;
-//     object-fit: cover;
-//     overflow: hidden;
-//     border: 2px solid #333;
-//     border-radius: 6px;
-// `
+import './style.css';
 
 const Post = props => {
     return(
