1296d0989386eef79d6b5e39f9a463f1d84bb166
Noel Nevarez <noel3740@gmail.com>
2019-03-25 21:33:32 -0500

    Adding span

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -44,7 +44,7 @@ class Nav extends React.Component {
                                 <div className="navbar-fixed">
                                     <nav id="navbar-nav" className="blue-grey darken-3">
                                         <div className="container nav-wrapper">
-                                            <Link to={"/"} className="brand-logo"><i className="logo"></i> Dad Overflow</Link>
+                                            <Link to={"/"} className="brand-logo"><i className="logo"></i><span> Dad Overflow</span></Link>
                                             <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                                             <ul className="right hide-on-med-and-down">
                                                 <li className={window.location.pathname.toLowerCase() === "/" ? "active" : ""}>
