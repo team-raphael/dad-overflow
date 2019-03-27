7721655aedc715dcdda2e88bfbf4a8351fb72aa3
Noel Nevarez <noel3740@gmail.com>
2019-03-27 09:05:29 -0500

    Removing author and replacing with a userId ref

--- models/comment.js
+++ models/comment.js
@@ -6,9 +6,9 @@ const Schema = mongoose.Schema;
 const commentSchema = new Schema({
  
   body: { type: String, required: true },
-  author: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  postId: { type: Schema.Types.ObjectId, ref: "Post", required: true }
+  postId: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 });
 //Models defined on the mongoose instance are available to all connection created
 const Comment = mongoose.model("Comment", commentSchema);
