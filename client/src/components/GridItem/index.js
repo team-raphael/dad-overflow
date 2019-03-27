e6205dd1b3041d3e9e035dadd3125c5b564ba7b6
Noel Nevarez <noel3740@gmail.com>
2019-03-26 19:57:39 -0500

    Changing look of the grid item component

--- client/src/components/GridItem/index.js
+++ client/src/components/GridItem/index.js
@@ -1,16 +1,45 @@
 import React from 'react'
 import './style.css';
-export const GridItem = props => {
+
+class GridItem extends React.Component {
+
+  state = {
+    isChecked: this.props.isComplete,
+  }
+
+  toggleCheckboxChange = () => {
+    const { handleCheckboxChange, taskId } = this.props;
+
+    this.setState(({ isChecked }) => (
+      {
+        isChecked: !isChecked,
+      }
+    ));
+
+    handleCheckboxChange(taskId, this.props.isComplete);
+  }
+
+  render() {
     return (
-      <div className={`item-box ${props.boxColor}`}>
-        <h6>{props.body}</h6>
-        <div className="item-box-inner-grid">
-        <i onClick={props.handleTaskComplete} data-iscomplete={props.isComplete} id={props.id} className={`${props.leftIcon} tooltipped`} data-position="top" data-tooltip="I am a tooltip"></i>
-        <i onClick={props.handleTaskDelete}  id={props.id} className={props.rightIcon}></i>            
+      <div id={ this.props.id } className="gridItemContainer row scale-transition">
+        <div>
+          <div className="card hoverable">
+            <div className="card-content left-align">
+              <div>
+                <label>
+                  <input type="checkbox" className="filled-in" checked={this.state.isChecked} onChange={this.toggleCheckboxChange} />
+                  <span className={this.props.isComplete ? "isComplete" : ""}>{this.props.body}</span>
+                </label>
+              </div>
+              <div className="actionButtonContainer right-align">
+                <button id={this.props.id}  onClick={this.props.handleTaskDelete} className="waves-effect waves-light btn">Delete <i className="material-icons right">delete</i></button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    )
-    
+    );
+  }
 }
 
 export default GridItem
