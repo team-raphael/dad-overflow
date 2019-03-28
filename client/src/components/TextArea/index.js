21efe6c6333e3a0eb3cbb2cc5b142250d469dd99
Noel Nevarez <noel3740@gmail.com>
2019-03-28 08:09:36 -0500

    Fixing display issue with comment reply on mobile

--- client/src/components/TextArea/index.js
+++ client/src/components/TextArea/index.js
@@ -6,9 +6,9 @@ export const TextArea = props => {
     return (
         <div className="text-div">
             <div className="row">
-                <form className="col l12">
+                <form className="col s12">
                     <div className="row">
-                        <div className="input-field col l9">
+                        <div className="input-field col s12">
                             <textarea id="textarea1" value={props.value} name={props.name} className="materialize-textarea" onChange={props.handleInputChange} />
                             <label htmlFor="textarea1">{props.label}</label>
                             <h6 className="red-error">{props.error}</h6>
