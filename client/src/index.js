34d7141127e0984f73927c639474de44c26540ef
Noel Nevarez <noel3740@gmail.com>
2019-03-27 21:54:12 -0500

    Removing usused animate import.

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Animate from 'animate.css';
 import 'animate.css/animate.css'
 
 
