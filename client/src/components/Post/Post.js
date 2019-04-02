d3e0c3c4e8fce7df8279a99b5ff086cbeeec0621
Hero's Dad <cody.monta@gmail.com>
2019-04-01 20:30:16 -0500

    added hover effect on comments text

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -6,21 +6,45 @@ import API from "../../services/APIService";
 
 export class Post extends React.Component {
   state = {
-    lengthOfComments: ""
+    lengthOfComments: "",
+    commentUsers: []
   };
 
   componentDidMount() {
-    API.getCommentsByPostId(this.props.postId).then(res => this.setState({ lengthOfComments: res.data.length }));
+    this.refreshCommentData();
+    var elems = document.querySelectorAll('.tooltipped');
+    window.M.Tooltip.init(elems);
+
   };
 
   componentDidUpdate = (prevProps) => {
 
     if (prevProps.postId !== this.props.postId) {
-      API.getCommentsByPostId(this.props.postId).then(res => this.setState({ lengthOfComments: res.data.length }));
+      this.refreshCommentData();
     }
+
+  }
+
+  refreshCommentData = () => {
+    API.getCommentsByPostId(this.props.postId).then(res => 
+      {
+        let userDisplayNames = res.data.map(res => res.userId.displayName);
+        if (userDisplayNames && userDisplayNames.length > 10) {
+          userDisplayNames = userDisplayNames.splice(0, 10);
+        }
+        console.log(userDisplayNames);
+
+        this.setState({ 
+          lengthOfComments: res.data.length,
+          commentUsers: [...new Set(userDisplayNames)]
+        });
+      });
   }
 
   render() {
+    
+
+
     return (
       // <div className='aos-init aos-animate' data-aos={props.isEven ? "fade-right" : 'fade-left'}>
       <div data-aos="flip-up" data-aos-offset="110">
@@ -50,8 +74,10 @@ export class Post extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="col s12 m3 comment-length right-align hide-on-small-only">{this.state.lengthOfComments === 1 ? `${'1 comment'}` : `${this.state.lengthOfComments} ${'comments'}`}</div>
+            <Link to={`/postdetail/${this.props.postId}`}>
+            <div data-postid={this.props.postId} className="col s12 m3 comment-length right-align hide-on-small-only tooltipped" data-position="top" data-tooltip={this.state.lengthOfComments < 1 ? "no comments" : this.state.commentUsers.join('<br/>') }>{this.state.lengthOfComments === 1 ? `${'1 comment'}` : `${this.state.lengthOfComments} ${'comments'}`}</div>
             <div className="col s12 small-comment left-align hide-on-med-and-up">{this.state.lengthOfComments === 1 ? `${'1 comment'}` : `${this.state.lengthOfComments} ${'comments'}`}</div>
+          </Link>
           </div>
         </div>
       </div>
--- client/src/components/PostCollection/index.js
+++ client/src/components/PostCollection/index.js
@@ -39,4 +39,3 @@ export class PostCollection extends React.Component {
     );
   }
 }
-
